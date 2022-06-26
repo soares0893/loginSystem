214e9bee2e559b25d49659623122ddf20c6d7908
anderson soares <soares0893@gmail.com>
2022-06-25 23:12:17 -0300

    Created HTML and CSS

--- src/app/login-view/login-view.component.ts
+++ src/app/login-view/login-view.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginViewComponent implements OnInit {
 
+  email:String = "";
+  password:String = "";
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  login(){
+    if(this.email != "" && this.password != ""){
+      
+    } else {
+      alert("Preencha todos os campos!");
+    }
+  }
+
+  forgotPassword(){
+    if(this.email != "") {
+      console.log("Redefinição de senha por e-mail");
+    } else {
+      alert("Informe o e-mail!")
+    }
   }
 
 }
